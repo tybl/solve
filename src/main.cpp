6be653f2e91e9a6f9e9248f42f32453c8e552a7c
Timothy Lyons <tlyons@srcinc.com>
2023-01-04 15:28:58 -0500

    Add token types

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,58 @@
 #include <iostream>
 #include <iterator>
 
+enum TokenType {
+	LEFT_PAREN,
+	RIGHT_PAREN,
+	LEFT_BRACE,
+	RIGHT_BRACE,
+	COMMA,
+	DOT,
+	MINUS,
+	PLUS,
+	SEMICOLON,
+	SLASH,
+	STAR,
+
+	BANG,
+	BANG_EQUAL,
+	EQUAL,
+	EQUAL_EQUAL,
+	GREATER,
+	GREATER_EQUAL,
+	LESS,
+	LESS_EQUAL,
+
+	IDENTIFIER,
+	STRING,
+	NUMBER,
+
+	AND,
+	CLASS,
+	ELSE,
+	FALSE,
+	FUN,
+	FOR,
+	IF,
+	NIL,
+	OR,
+	PRINT,
+	RETURN,
+	SUPER,
+	THIS,
+	TRUE,
+	VAR,
+	WHILE,
+	END_OF_FILE
+};
+
+class Token {
+	TokenType type;
+	std::string lexeme;
+	// object literal;
+	int line;
+};
+
 void run(std::string input) {
 	std::cout << "Input: $" << input << "$ end of input\n";
 }
